3f2445eafcf588d307894c166078217fcb8a1c4d
Michu258 <67603587+Michu258@users.noreply.github.com>
2020-07-26 09:38:10 +0200

    Third commit

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    cout << " Hello, World!" << endl;
     cout << " 1st" << endl;
     cout << " Second commit" << endl;
+    cout << " Third commit" << endl;
     return 0;
 }
